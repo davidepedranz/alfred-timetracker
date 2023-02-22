80dcf73cf44570fd0665066ea19d9df0c9860c8e
Davide Pedranz <davide.pedranz@gmail.com>
2023-02-22 19:53:13 +0100

    chore: migrate from Travis to GitHub Actions for CI

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o nounset
 
-archive="TimeTracker-${VERSION}.alfredworkflow"
+archive="TimeTracker-${TIMETRACKER_VERSION}.alfredworkflow"
 
 echo "Building go binaries:"
 GOARCH=amd64 GOOS=darwin go build -ldflags "-s -w" -o ".workflow/tt-amd64" .
@@ -13,6 +13,7 @@ echo ""
 echo "Crearing archive:"
 (
   cd ./.workflow || exit
+  export TIMETRACKER_VERSION_WITHOUT_PREFIX=$(echo "${TIMETRACKER_VERSION}" | sed 's/^v//')
   envsubst >./info.plist <./info.plist.template
   zip -r "../${archive}" ./*
   zip -d "../${archive}" info.plist.template
